c53822333d571d798ed6d5c53d2be39c3bf60e25
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2022-01-13 06:54:26 -0500

    finished first version

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
+//! Linux supports some standard ioctls to configure network devices.
+//! They can be used on any socket's file descriptor regardless of
+//! the family or type. Most of them pass an ifreq structure.
+//! Source: netdevice(7)
 use std::os::unix::prelude::RawFd;
-
-// Linux supports some standard ioctls to configure network devices.
-// They can be used on any socket's file descriptor regardless of
-// the family or type. Most of them pass an ifreq structure.
-// Source: netdevice(7)
 use anyhow::{bail, Result};
 use ifstructs::ifreq;
 use nix::libc::{SIOCGIFADDR, SIOCSIFADDR};
 use nix::sys::socket::{socket, AddressFamily, SockFlag, SockProtocol, SockType};
 use nix::{ioctl_read_bad, ioctl_write_ptr_bad};
-use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
+use simple_logger::SimpleLogger;
+use std::net::{IpAddr, Ipv4Addr};
+use structopt::StructOpt;
+use log::info;
 
 /// Create an endpoint for communication
 fn crate_sock<T: Into<Option<SockProtocol>>>(
@@ -21,12 +23,12 @@ fn crate_sock<T: Into<Option<SockProtocol>>>(
     Ok(socket(domain, ty, flags, protocol)?)
 }
 
+// Creation of icotl functions needed
 ioctl_read_bad!(get_interface_ip, SIOCGIFADDR, ifreq);
 ioctl_write_ptr_bad!(set_interface_ip, SIOCSIFADDR, ifreq);
 
-fn get_ip(ifr: &ifreq) -> Result<IpAddr> {
-    let sock_addr = unsafe { ifr.ifr_ifru.ifr_addr };
-
+/// Get `IpAddr` from sockaddr
+pub fn ip_from_sockaddr(sock_addr: &libc::sockaddr) -> Result<IpAddr> {
     match sock_addr.sa_family as i32 {
         // IPV4
         libc::AF_INET => {
@@ -37,28 +39,68 @@ fn get_ip(ifr: &ifreq) -> Result<IpAddr> {
             Ok(IpAddr::from(Ipv4Addr::from(arr)))
         }
         // IPV6
-        libc::AF_INET6 => {
-            let mut arr = [0u16; 8];
-            for i in 0..arr.len() {
-                arr[i] = sock_addr.sa_data[i + 6] as u16;
-            }
-            Ok(IpAddr::from(Ipv6Addr::from(arr)))
-        }
+        libc::AF_INET6 => bail!("IPv6 is not supported at the time"),
         _ => bail!("Received unknown sa_family"),
     }
 }
 
+/// Get `sockaddr` from IpAddr
+pub fn sockaddr_from_ip(ip_addr: &IpAddr) -> Result<libc::sockaddr> {
+    let sa_family: libc::sa_family_t;
+    let mut sa_data = [0i8; 14];
+
+    match ip_addr {
+        IpAddr::V4(ip) => {
+            sa_family = libc::AF_INET as libc::sa_family_t;
+            let data = ip.octets();
+            for i in 0..data.len() {
+                sa_data[i + 2] = data[i] as i8;
+            }
+        }
+        _ => bail!("IPv6 is not supported at the time")
+    };
+
+    Ok(libc::sockaddr {
+        sa_family,
+        sa_data
+    })
+}
+
+// get the ip of interface
+pub fn get_ip(ifr: &ifreq) -> Result<IpAddr> {
+    ip_from_sockaddr(unsafe { &ifr.ifr_ifru.ifr_addr })
+}
+
+// set the ip of interface
+pub fn set_ip(ifr: &mut ifreq, ip_addr: &IpAddr) -> Result<()> {
+    Ok(ifr.ifr_ifru.ifr_addr = sockaddr_from_ip(ip_addr)?)
+}
+
+#[derive(Debug, StructOpt)]
+struct Args {
+    /// Interface to set IP
+    interface: String,
+
+    /// IPv4 to set
+    ip: Ipv4Addr,
+}
+
 fn main() -> Result<()> {
+    SimpleLogger::new().init()?;
+    let args = Args::from_args();
+
+    info!("Opening socket to kernel...");
     let sock_fd = crate_sock(
         AddressFamily::Inet,
         SockType::Datagram,
         SockFlag::empty(),
         None,
     )?;
-    let mut ifreq = ifreq::from_name("ens36")?;
 
-    unsafe { get_interface_ip(sock_fd, &mut ifreq)? };
-    let address = get_ip(&ifreq)?;
-    println!("Interface ip is: {:?}", address);
+    let mut ifreq = ifreq::from_name(&args.interface)?;
+    let new_addr = IpAddr::from(args.ip);
+    set_ip(&mut ifreq, &new_addr)?;
+    unsafe { set_interface_ip(sock_fd, &mut ifreq)? };
+    info!("Interface '{}' set to ip address '{}' succesfully!", args.interface, new_addr);
     Ok(())
 }
